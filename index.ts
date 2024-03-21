a07fd210212c495af54228cc0ec45d5786765452
ken koch <kkoch986@gmail.com>
2024-03-20 23:46:40 -0400

    feat: add ollama support

--- index.ts
+++ index.ts
@@ -13,10 +13,87 @@ type Response = {
   text: string,
 };
 
+type Message = {
+  role: string,
+  content: string,
+};
+
 interface Backend {
   start(): Promise<Response>,
   receive(message:string): Promise<Response>,
 };
+
+// OLlama backend is backed by ollama
+// the assumption is that relevant models have already been pulled
+class OLlamaBackend implements Backend {
+  private baseURL:string;
+  private model:string;
+  private systemPrompts:string[];
+  private initialPrompt: string;
+  private messages: Message[];
+
+  async initialize(baseURL: string, model: string, systemPrompts: string[], initialPrompt: string) {
+    this.model = model;
+    this.baseURL = baseURL;
+    this.systemPrompts = systemPrompts;
+    this.initialPrompt = initialPrompt;
+    // LATER: we could pull the model at this point, but that seems like not necessary at this point
+  }
+
+  async sendPrompt(message:string): Promise<Response> {
+    if (message != "") {
+      this.messages.push({
+        role: 'user',
+        content: message,
+      });
+    }
+    const p = await fetch(this.baseURL + "api/chat", {
+      method: "POST",
+      body: JSON.stringify({
+        "model": this.model,
+        // LATER: support streaming calls?
+        "stream": false,
+        "options": {
+          "temperature": 1
+        },
+        "messages": this.messages,
+      }),
+    })
+    .then((response) => response.json())
+    .then((resp: any) => {
+      // TODO: error handling
+      this.messages.push(resp.message);
+      const i = resp.created_at;
+      return {
+        id: i,
+        characterId: this.model,
+        chatId: i,
+        imageRelativePath: "",
+        srcName: "ollama-" + this.model,
+        text: resp.message.content,
+      };
+    });
+    return p;
+  }
+
+  async start(): Promise<Response> {
+    // send the basic system prompts and return the response
+    const messages = this.systemPrompts.map((m) => ({
+      "role": "system",
+      "content": m,
+    }));
+    messages.push({
+      "role": "user",
+      "content": this.initialPrompt,
+    });
+    this.messages = messages;
+    return this.sendPrompt("");
+  }
+
+  async receive(message: string): Promise<Response> {
+    return this.sendPrompt(message);
+  }
+}
 
 // Echo backend is just a testing backend that replies with exactly
 // the message it was sent
@@ -140,6 +217,21 @@ class CharacterAIBackend implements Backend {
   switch (backendType) {
     case "echo":
       backend = new EchoBackend();
+      break ;
+    case "ollama":
+      const ol = new OLlamaBackend();
+    // TODO: better parameterize all of this...
+      await ol.initialize(
+        "http://ollama:11434/",
+        "neural-chat",
+        [
+          "you are a zoltar inspired fortune teller with a tiki / bayou flair",
+          "your name is Makani",
+          "you should always respond to prompts with a whimsical style",
+        ],
+        "please introduce yourself",
+      );
+      backend = ol;
       break ;
     case "characterai":
       const cai = new CharacterAIBackend();
